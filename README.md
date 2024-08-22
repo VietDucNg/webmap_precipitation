95d9f845f3b003ee88ad87dc1ef4bda261b89a2b
VietDucNg <142153769+VietDucNg@users.noreply.github.com>
2024-08-22 11:56:44 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# test
- Real-time US precipitation and weather alert: data from National Weahter Service
+<img src='img/proj_overview.png' alt='project overview image'>
+
+<h1> Precipitation webmap </h1>
+ 
+ <p>The webmap provide real-time US precipitation and weather alert from National Weahter Service.</p>
+
+ > ℹ️ Access the webmap via this <a href =''>link</a>
+
+<h2>Credit</h2>
+<p> 👨‍🏫 The webmap developed by <a href='https://vietducng.github.io/'>Viet Nguyen</a> (<a href = 'https://geo.uni-greifswald.de/en/chairs/geographie/translate-to-english-fernerkundung-und-geoinformationsverarbeitung/translate-to-english-team/'>University of Greifswald | Institute of Geography and Geology</a>) with reference to materials from the <a href='https://geog4046.github.io/'> GEOG 4046 An Introduction to Web GIS</a> course.</p>
+
+<h2>Data</h2>
+
+<h3>🌧️ Precipitation layer</h3>
+<p> 
+- <strong>Layer type</strong>: OGC Web Map Service <br> 
+- <strong>Summary</strong>: A National Weather Service image product depicting current precipitation derived from a network of radar stations. The web service is provided by <a href='https://mesonet.agron.iastate.edu/ogc/'>Iowa State University Mesonet</a>. <br> 
+- <strong>Service URL</strong>: <a href='https://www.weather.gov/documentation/services-web-api#/default/get_alerts_active'> <i> https://www.weather.gov/documentation/services-web-api#/default/get_alerts_active </i></a><br> 
+</p>
+
+<h3> 🌪️ Weather alerts layer</h3>
+<p> 
+- <strong>Layer type</strong>: GeoJSON <br> 
+- <strong>Summary</strong>: Areas under a weather alert issued by the National Weather Service, such as a tornado warning. See the NWS for <a href = 'https://www.weather.gov/documentation/services-web-api#/default/get_alerts_active'>details on the API</a>. <br> 
+- <strong>Service URL</strong>: <a href='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'> <i> https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png </i></a><br> 
+</p>
---
+++ scripts/main.js
@@ -0,0 +1,104 @@
+// create map object with initial view and zoom level
+var map = L.map('map').setView([38, -95], 4);
+
+//// add basemap layer to map
+var base_carto = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png',{
+  maxZoom: 20,
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+  subdomains: 'abcd'}
+).addTo(map);
+
+var base_gg = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',{
+  maxZoom: 20,
+  subdomains: ['mt0','mt1','mt2','mt3']
+});
+
+
+//// add precipitation radar layer to map
+/*
+Title: NEXRAD Base Reflectivity Current
+Layer type: OGC Web Map Service
+Summary: A National Weather Service image product depicting current precipitation derived from a network of radar stations. The web service is provided by Iowa State University Mesonet.
+https://mesonet.agron.iastate.edu/ogc/
+*/
+
+var radarUrl = 'https://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi';
+var radarDisplayOptions = {
+  layers: 'nexrad-n0r-900913',
+  format: 'image/png',
+  transparent: true
+};
+var precipitation = L.tileLayer.wms(radarUrl, radarDisplayOptions).addTo(map);
+
+
+//// add weather alerts layer to map
+/*
+Weather alerts
+Title: National Weather Service Active Alerts
+Layer type: GeoJSON
+Summary: Areas under a weather alert issued by the National Weather Service, such as a tornado warning. See the NWS for details on the API.
+https://www.weather.gov/documentation/services-web-api#/default/get_alerts_active
+*/
+
+// create empty layer to store JSON data from JQuery request
+// this is a workaround to add a GeoJSON layer from AJAX through jQuery
+// to leaflet layer control
+var weatherAlerts = L.layerGroup().addTo(map);
+
+// use JQuery function to request data in JSON format
+var weatherAlertsUrl = 'https://api.weather.gov/alerts/active?region_type=land';
+
+$.getJSON(weatherAlertsUrl, function(data) {
+  L.geoJSON(data,{
+    // style alert polygon based on severity
+    style: function (feature) {
+      var color = 'orangered';
+      var fillColor = 'darkorange';
+      var opacity = 0.5;
+      if (feature.properties.severity === 'Severe') {
+        color ='darkred';
+        fillColor ='crimson';
+        opacity = 0.8;
+      }
+      return { color: color, weight: 2, fillColor: fillColor, 
+        fillOpacity: opacity}
+    },
+    onEachFeature: function(feature, layer) {
+      // add popup with alert details when clicked
+      layer.bindPopup(feature.properties.headline);
+      // add every feature found in JSON data to weatherAlerts layer
+      weatherAlerts.addLayer(layer);
+    }
+  });
+});
+
+
+// add popup with coordinates when clicked
+var popup = L.popup();
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent(e.latlng.lat.toFixed(6).toString() + ', ' +
+        e.latlng.lng.toFixed(6).toString())
+        .openOn(map);
+}
+
+map.on('click', onMapClick);
+
+
+//// add layers control
+// create control object 
+var baseLayers = {
+  'CartoDB Light': base_carto,
+  'Google Satellite': base_gg
+};
+
+var overlayMaps = {
+  'Precipitation': precipitation,
+  'Weather Alerts': weatherAlerts
+};
+// add layers control to map
+var layersControl = L.control.layers(baseLayers, overlayMaps).addTo(map);
+
+// add scale control to map
+L.control.scale().addTo(map);
